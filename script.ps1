0b8521fa85ef505444e5589f5d6f562203916090
DeVogelRyan <devogelryan2@gmail.com>
2023-04-17 11:32:13 +0200

    added log write

--- script.ps1
+++ script.ps1
@@ -1,6 +1,19 @@
 $response = Invoke-RestMethod -Uri "https://api.coindesk.com/v1/bpi/currentprice.json"
-$response | Get-Member
+#$response | Get-Member
 $updatedTime =  $response.time.updated
-$rate = $response.bpi.EUR.rate_float
+$rateUSD = $response.bpi.USD.rate_float
+$rateGBP = $response.bpi.GBP.rate_float
+$rateEUR = $response.bpi.EUR.rate_float
 
-Write-Host $rate
+Write-Host $rateGBP
+
+$Logfile = "C:\PS\computername.log"
+function WriteLog
+{
+Param ([string]$LogString)
+$Stamp = (Get-Date).toString("yyyy/MM/dd HH:mm:ss")
+$LogMessage = "$Stamp $LogString"
+Add-content $LogFile -value $LogMessage
+}
+
+WriteLog 
