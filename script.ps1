3185fbb7318e66b82223fd37f0e7a41d02c6e437
DeVogelRyan <devogelryan2@gmail.com>
2023-04-17 12:03:49 +0200

    added functions for request and made parameters mandatory

--- script.ps1
+++ script.ps1
@@ -1,19 +1,29 @@
-$response = Invoke-RestMethod -Uri "https://api.coindesk.com/v1/bpi/currentprice.json"
-#$response | Get-Member
-$updatedTime =  $response.time.updated
-$rateUSD = $response.bpi.USD.rate_float
-$rateGBP = $response.bpi.GBP.rate_float
-$rateEUR = $response.bpi.EUR.rate_float
-
-Write-Host $rateGBP
 
 $Logfile = "C:\PS\computername.log"
-function WriteLog
-{
-Param ([string]$LogString)
-$Stamp = (Get-Date).toString("yyyy/MM/dd HH:mm:ss")
-$LogMessage = "$Stamp $LogString"
-Add-content $LogFile -value $LogMessage
+function WriteLog {
+    param(
+        # Forcing the need for this parameter.
+        [Parameter(Position = 0, mandatory = $true)]
+        [System.Object] $BTCData)
+    $Stamp = (Get-Date).toString("yyyy/MM/dd HH:mm:ss")
+    $LogMessage = $Stamp + " " + $BTCData.rateUSD
+    Add-content $LogFile -value $LogMessage
+
+    Write-Host "object:" $LogMessage
 }
 
-WriteLog 
+function RequestAPI {
+    Param ([string]$url)
+    $response = Invoke-RestMethod -Uri $url
+    #pass properties to object
+    $data = [PSCustomObject]@{
+        updatedTime = $response.time.updated
+        rateUSD     = $response.bpi.USD.rate_float
+        rateGBP     = $response.bpi.GBP.rate_float
+        rateEUR     = $response.bpi.EUR.rate_float
+    }
+
+    WriteLog $data
+}
+
+RequestAPI "https://api.coindesk.com/v1/bpi/currentprice.json"
