14ca55a7f21e64c5b97999d63bf42389b05ced84
DeVogelRyan <devogelryan2@gmail.com>
2023-04-17 15:06:06 +0200

    added .env file

--- script.ps1
+++ script.ps1
@@ -1,29 +1,32 @@
-
-$Logfile = "C:\PS\computername.log"
 function WriteLog {
     param(
         # Forcing the need for this parameter.
         [Parameter(Position = 0, mandatory = $true)]
         [System.Object] $BTCData)
-    $Stamp = (Get-Date).toString("yyyy/MM/dd HH:mm:ss")
-    $LogMessage = $Stamp + " " + $BTCData.rateUSD
+    $Logfile = "C:\PS\computername.csv"
+    $LogMessage = '"{0}", "{1}", "{2}", "{3}"' -f $BTCData.updatedTime, $BTCData.rateUSD, $BTCData.rateGBP, $BTCData.rateEUR
+    if (-Not (Test-Path $Logfile)) {
+        Add-content $LogFile -value "UpdatedAt, USDRate, BGPRate, EURRate"
+    }
     Add-content $LogFile -value $LogMessage
-
-    Write-Host "object:" $LogMessage
 }
 
 function RequestAPI {
-    Param ([string]$url)
-    $response = Invoke-RestMethod -Uri $url
-    #pass properties to object
-    $data = [PSCustomObject]@{
-        updatedTime = $response.time.updated
-        rateUSD     = $response.bpi.USD.rate_float
-        rateGBP     = $response.bpi.GBP.rate_float
-        rateEUR     = $response.bpi.EUR.rate_float
-    }
+    # retrieve URL from .env file
+    get-content .env | foreach {
+        $name, $value = $_.split('=')
+        Write-Host $value
+        $response = Invoke-RestMethod -Uri $value
+        #pass properties to object
+        $data = [PSCustomObject]@{
+            updatedTime = $response.time.updated
+            rateUSD     = $response.bpi.USD.rate_float
+            rateGBP     = $response.bpi.GBP.rate_float
+            rateEUR     = $response.bpi.EUR.rate_float
+        }
 
-    WriteLog $data
+        WriteLog $data
+    }
 }
 
-RequestAPI "https://api.coindesk.com/v1/bpi/currentprice.json"
+RequestAPI
